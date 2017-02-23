9291c73dbeed5595b35bd167af0af3d0fff9ece0
Mikhail Morozov <mcm_@mail.ru>
2017-02-23 18:24:17 +0200

    Start project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shurup1.com/store/admin/');
+define('HTTP_CATALOG', 'http://shurup1.com/store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shurup1.com/store/admin/');
+define('HTTPS_CATALOG', 'http://shurup1.com/store/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/shurup1/shurup1.com/store/admin/');
+define('DIR_SYSTEM', '/var/www/shurup1/shurup1.com/store/system/');
+define('DIR_IMAGE', '/var/www/shurup1/shurup1.com/store/image/');
+define('DIR_LANGUAGE', '/var/www/shurup1/shurup1.com/store/admin/language/');
+define('DIR_TEMPLATE', '/var/www/shurup1/shurup1.com/store/admin/view/template/');
+define('DIR_CONFIG', '/var/www/shurup1/shurup1.com/store/system/config/');
+define('DIR_CACHE', '/var/www/shurup1/shurup1.com/store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/shurup1/shurup1.com/store/system/storage/download/');
+define('DIR_LOGS', '/var/www/shurup1/shurup1.com/store/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/shurup1/shurup1.com/store/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/shurup1/shurup1.com/store/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/shurup1/shurup1.com/store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'u_shurup1');
+define('DB_PASSWORD', 'f3hlIsUB');
+define('DB_DATABASE', 'shurup1');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'store_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shurup1.com/store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shurup1.com/store/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/shurup1/shurup1.com/store/catalog/');
+define('DIR_SYSTEM', '/var/www/shurup1/shurup1.com/store/system/');
+define('DIR_IMAGE', '/var/www/shurup1/shurup1.com/store/image/');
+define('DIR_LANGUAGE', '/var/www/shurup1/shurup1.com/store/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/shurup1/shurup1.com/store/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/shurup1/shurup1.com/store/system/config/');
+define('DIR_CACHE', '/var/www/shurup1/shurup1.com/store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/shurup1/shurup1.com/store/system/storage/download/');
+define('DIR_LOGS', '/var/www/shurup1/shurup1.com/store/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/shurup1/shurup1.com/store/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/shurup1/shurup1.com/store/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'u_shurup1');
+define('DB_PASSWORD', 'f3hlIsUB');
+define('DB_DATABASE', 'shurup1');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'store_');
